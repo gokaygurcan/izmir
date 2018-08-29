925961cca40153ebdb1e4c11e3df987f2b412084
Hilmi Tolga Sahin <hsahin@hwj01083.pvhcorp.com>
2018-08-29 15:09:14 +0200

    Wrap governor test

--- src/lib/Governor.test.js
+++ src/lib/Governor.test.js
@@ -1,12 +1,14 @@
 import Governor from './Governor'
-test('Governor test with custom config',() => {
-    const governor = new Governor({
-			MODULES: process.cwd()+'/.git/super-modules'
-		})
-    const governorConfig = governor.getConfig()
-    const {PREFIX}=governorConfig
-    const temp = governor.getModulePaths().filter(modName => {
-        return modName.indexOf(PREFIX) === 0
-    })
-    console.log(temp)
+describe('Governor tests',()=>{
+				test('Governor test with custom config',() => {
+						const governor = new Governor({
+							MODULES: process.cwd()+'/.git/super-modules'
+						})
+						const governorConfig = governor.getConfig()
+						const {PREFIX}=governorConfig
+						const temp = governor.getModulePaths().filter(modName => {
+								return modName.indexOf(PREFIX) === 0
+						})
+						console.log(temp)
+				})
 })
