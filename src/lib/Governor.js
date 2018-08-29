7c6639c34f29c51be452c67752aa413227a79328
Hilmi Tolga Sahin <hsahin@hwj01083.pvhcorp.com>
2018-08-29 14:59:50 +0200

    Ensure the userconfig modules folder

--- src/lib/Governor.js
+++ src/lib/Governor.js
@@ -3,17 +3,18 @@ import fs from 'fs'
 
 export default class Governor {
     constructor(userConfig) {
+				// Make sure module path is properly passed
+				if(userConfig.MODULES) {
+					userConfig.MODULES = path.resolve(userConfig.MODULES)
+				}
         this.userConfig = userConfig
     }
-
     getConfig() {
-
         const defaultConfig = {
             PREFIX: 'super',
             MODULES: path.resolve(process.cwd(), 'node_modules'),
             PORT: 8080
         }
-
         // userConfig overwrites on defaults
         const config = Object.assign({}, defaultConfig, this.userConfig)
         return config
@@ -31,6 +32,6 @@ export default class Governor {
         return paths
     }
     loadModules() {
-
+			console.log(`${this.getModulePaths()} modules will be loaded`)
     }
 }
--- src/lib/Governor.test.js
+++ src/lib/Governor.test.js
@@ -1,9 +1,10 @@
 import Governor from './Governor'
-test('Governor test',() => {
-    const governor = new Governor()
+test('Governor test with custom config',() => {
+    const governor = new Governor({
+			MODULES: process.cwd()+'/.git/super-modules'
+		})
     const governorConfig = governor.getConfig()
     const {PREFIX}=governorConfig
-    
     const temp = governor.getModulePaths().filter(modName => {
         return modName.indexOf(PREFIX) === 0
     })
