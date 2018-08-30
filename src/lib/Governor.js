e2f70e884143ce1f8b4bcfba607cd4506483ce8a
Hilmi Tolga Sahin <hsahin@hwj01083.pvhcorp.com>
2018-08-30 17:28:59 +0200

    Load environment variables from .env with dotenv

--- src/lib/Governor.js
+++ src/lib/Governor.js
@@ -1,45 +1,51 @@
-import path from 'path'
-import fs from 'fs'
+import path from 'path';
+import fs from 'fs';
+
+// Load Environment variables
+require('dotenv').config();
 
 export default class Governor {
-    constructor(userConfig) {
-				// Initiate the config	
-				this.config = {
-					PREFIX:process.env.IZMIR_PREFIX || 'izmir',
-					PORT:process.env.IZMIR_PORT || 8080,
-					MODULES:process.env.IZMIR_MODULES || 'node_modules'
-				}
-		}
-		getConfig() {
-			return this.config
-		}
-    getPlugins() {
-				const {
-					MODULES,
-					PREFIX
-				} = this.config
-
-        const paths = fs.readdirSync(MODULES)
-        		.filter(modPath => {
-                return modPath.indexOf(PREFIX) === 0
-            })
-						.map(p => {
-							return {
-								path:path.resolve(`${MODULES}/${p}`),
-								name:p
-							}
-						})
-        return paths
-    }
-    loadPlugins() {
-			this.getPlugins.forEach(modulePath => {
-								const p = path.resolve(`${MODULES}/${modulePath}/index.js`)
-								if(!fs.existsSync(p)){
-									throw new Error(`Module ${modulePath} does not have index.js as entry file!`)
-								} else {
-									require(p)()
-								}
-						})
-		}
+  constructor() {
+    const {PLUGIN_PATH, PORT} = process.env;
+    // Initiate the config
+    this.config = {
+      PREFIX: 'izmir',
+      PLUGIN_PATH,
+      PORT,
+    };
+  }
+  getConfig() {
+    return this.config;
+  }
+  getInfo() {
+    console.log(`-------- Governor for Izmir CMS Info --------`);
+    console.log(`Plugin prefix : ${this.config.PREFIX}-`)
+		console.log(`Plugins folder : ${this.config.PLUGIN_PATH}`)
+  }
+  getPlugins() {
+    const paths = fs
+      .readdirSync(this.config.PLUGIN_PATH)
+      .filter(pluginDirName => {
+        return pluginDirName.indexOf(this.config.PREFIX) === 0;
+      })
+      .map(pluginDir => {
+        return {
+          path: path.resolve(`${this.config.PLUGIN_PATH}/${pluginDir}`),
+          name: pluginDir,
+        };
+      });
+    return paths;
+  }
+  loadPlugins() {
+    this.getPlugins().forEach(plugin => {
+      const p = path.resolve(`${plugin.path}/index.js`);
+      if (!fs.existsSync(p)) {
+        throw new Error(
+          `Module ${plugin.path} does not have index.js as entry file!`,
+        );
+      } else {
+        require(p)();
+      }
+    });
+  }
 }
-
--- src/lib/Governor.test.js
+++ src/lib/Governor.test.js
@@ -1,10 +1,10 @@
-import Governor from './Governor'
-describe('Governor tests',()=>{
-				test('Governor test with custom config',() => {
-						const governor = new Governor({
-							MODULES: process.cwd()+'/.git/super-modules'
-						})
-						const governorConfig = governor.getConfig()
-						console.log(governor.getPlugins()) 
-				})
-})
+import Governor from './Governor';
+describe('Governor tests', () => {
+  test('Governor test with custom config', () => {
+    const governor = new Governor();
+		governor.getInfo()
+    const governorConfig = governor.getConfig();
+    console.log(governor.getPlugins())
+		governor.loadPlugins()
+  });
+});
