fdb1b71bf0732e48299c463984cc33f2655b10b3
HILMI SAHIN <htolgasahin@gmail.com>
2018-09-23 00:47:52 +0200

    Added auth route

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 const router = express.Router()
 import bodyParser from 'body-parser'
-console.log(bodyParser)
-router.use(bodyParser)
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
+
 // define the home page route
 router.post('/login', function (req, res) {
+    console.log(req.body)
     res.send('Birds home page')
 })
 router.post('/validate', function (req, res) {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
-import express from 'express';
+import express from 'express'
+
+// Routes
+import auth from './auth'
 
 const {PORT} = process.env
-
 const app = express()
+
+app.use('/auth',auth)
+
 app.get('/_', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
