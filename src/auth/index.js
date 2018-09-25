bbb58a49f56c8c9743fffe5cdfdd2f0543c55860
Hilmi Tolga SAHIN <htolgasahin@gmail.com>
2018-09-25 11:56:01 +0000

    Check if username exists on register

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
-
+const Database = require("../../Database") 
+const userCollection = new Database("users")
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
@@ -13,8 +14,14 @@ router.post('/login', function (req, res) {
 router.post('/validate', function (req, res) {
     res.send('Birds home page')
 })
-router.post('/register', function (req, res) {
-    res.send('Birds home page')
+router.post('/register', async (req, res)=> {
+    const {username} = req.body
+		// check if username exists
+	  const usersfound = await userCollection.find({username}) 
+		if(usersfound.length > 0){
+			res.send({status:false})
+		}
+		res.send('Birds home page')
 })
 
 module.exports = router
